3c83c6731cfec8cb5153d29794fda076a7d87db8
denismirchev <denis.g.mirchev@gmail.com>
2025-02-23 09:03:29 +0200

    Added route for geojson files

--- services/tile-server/main.py
+++ services/tile-server/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
 
 # Path to your pre-generated tiles
 TILE_DIR = os.path.join(os.getcwd(), "tiles")
+GEOJSON_DIR = os.path.join(os.getcwd(), "geojsons")
 
 #TODO: Add way to collect initial satellite imagery
 
@@ -30,8 +31,18 @@ async def serve_tile(dam_id: str, year: int, month: int, z: int, x: int, y: int)
         raise HTTPException(status_code=404, detail="Tile Not Found")
 
 
+@app.get("/tiles/{dam_id}/{year}/{month}/geojson")
+async def serve_geojson(dam_id: str, year: int, month: int):
+    """Serves GeoJSON files from the tile directory."""
+    geojson_path = os.path.join(GEOJSON_DIR, dam_id, f'{year}_{month}.geojson')
+
+    if os.path.exists(geojson_path):
+        return FileResponse(geojson_path)
+    else:
+        raise HTTPException(status_code=404, detail="GeoJSON Not Found")
+
 # Run the application with Uvicorn (recommended for FastAPI)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
