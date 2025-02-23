7ad801f6dcb101d5bfc78d1b15354cdabf486199
IvaAMarinova <ivamarinova06@gmail.com>
2025-02-23 03:34:00 +0200

    Added dam interface, fixed map-1 popup component with hardcoded measurements.

--- my-app/interfaces/dam.interface.ts
+++ my-app/interfaces/dam.interface.ts
@@ -1,9 +1,32 @@
 import Node from "./node.interface";
 
+enum Tendency {
+	UP = "Повишава се",
+	NO_CHANGE = "Без промяна",
+	DOWN = "Понижава се",
+}
+
+interface DamMeasurements {
+	timestamp: Date;
+	avg_incoming_flow: number | null;
+	avg_outgoing_flow: number | null;
+	fill_volume: number | null;
+	tendency: Tendency | null;
+	id: string;
+}
+
 interface Dam extends Node {
-	borderGeometry: [number, number][];
-	maxVolume: number;
-	description: string;
+	border_geometry: [number, number][] | null;
+	max_volume: number | null;
+	description: string | null;
+	municipality: string | null;
+	owner: string | null;
+	owner_contact: string | null;
+	operator: string | null;
+	operator_contact: string | null;
+	places: Record<string, string>;
+	measurements: DamMeasurements[] | null;
 }
 
 export default Dam;
+export { Tendency };
