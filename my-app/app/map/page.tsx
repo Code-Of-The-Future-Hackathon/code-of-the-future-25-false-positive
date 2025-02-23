7b67c3718d462adb1b4d9ecc9cc53004ae50a065
denismirchev <denis.g.mirchev@gmail.com>
2025-02-23 05:40:03 +0200

    Updated map stuff

--- my-app/app/map/page.tsx
+++ my-app/app/map/page.tsx
@@ -4,6 +4,11 @@ import MapVisuals from "@/app/map/MapVisuals";
 const MapPage = async () => {
 	const response = await fetch("http://localhost:8000/dams?skip=0&limit=10");
 	const dams = await response.json();
+
+	const res2 = await fetch(
+		"http://localhost:8000/dams/feb0577f-335b-4516-a148-21d27f40ad5e",
+	);
+	dams.push(await res2.json());
 	return <MapVisuals dams={dams} />;
 };
 
